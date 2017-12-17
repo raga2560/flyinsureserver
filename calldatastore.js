9750b51273d8d62eff1e180cce9248f8d3850f8a
Ramesh BN <raga2560@yahoo.com>
2017-12-17 18:07:50 +0530

    get working with mongo

--- calldatastore.js
+++ calldatastore.js
@@ -1,56 +1,58 @@
+var mongo = require('mongodb');
 
-function CallDataStore(db, mycoll) {
+
+function CallDataStore(db) {
     "use strict";
 
     /* If this constructor is called without the "new" operator, "this" points
      * to the global object. Log a warning and call it correctly. */
     if (false === (this instanceof CallDataStore)) {
         console.log('Warning: SportsDAO constructor called without "new" operator');
-        return new CallDataStore(db, mycoll);
+        return new CallDataStore(db);
     }
 
-    var dbtodos = db.collection(mycoll);
+    //var dbtodos = db.collection(mycoll);
 
 
 
 var todos = {
  
-    getAllTodos: function(callback) {
-         dbtodos.find().toArray(callback);
+    listall: function(recname,query, callback) {
+		var dbtodos1 = db.collection(recname);
+         dbtodos1.find(query).toArray(callback);
     },
-    listall: function(callback) {
-         dbtodos.find().toArray(callback);
-    },
-    push: function(todo, callback) {
-        dbtodos.insert(todo, callback);
+    push: function(recname, todo, callback) {
+		var dbtodos1 = db.collection(recname);
+        dbtodos1.insert(todo, callback);
 		
     },
-    saveTodo: function(todo, callback) {
-        dbtodos.insert(todo, callback);
-    },
-    get: function(todo, callback) {
-        dbtodos.find({
-            _id: todo.id
+    get: function(recname, id, callback) {
+		console.log('in get call'+id +'  '+recname);
+		var o_id = new mongo.ObjectID(id);
+		
+		var dbtodos1 = db.collection(recname);
+        dbtodos1.findOne({
+            _id: o_id,
         },  callback);
     },
-    update: function(todo, callback) {
-        dbtodos.update({
-            _id: todo.id
-        }, todo, {}, callback);
+    update: function(recname, id, data, callback) {
+		var dbtodos1 = db.collection(recname);
+		var o_id = new mongo.ObjectID(id);
+        dbtodos1.update({
+            _id: o_id
+        }, data, {}, callback);
     },
-    updateTodo: function(todo, callback) {
-        dbtodos.update({
-            _id: todo.id
-        }, todo, {}, callback);
-    },
-    remove: function(id, callback) {
-        dbtodos.remove({
-            _id: id
+    remove: function(recname, id, callback) {
+		var dbtodos1 = db.collection(recname);
+		var o_id = new mongo.ObjectID(id);
+        dbtodos1.remove({
+            _id: o_id
         }, '', callback);
     },
-    deleteTodo: function(id, callback) {
-        dbtodos.remove({
-            id: id
+	removeall: function(recname, callback) {
+		var dbtodos1 = db.collection(recname);
+        dbtodos1.remove({
+            
         }, '', callback);
     }
 }
--- routes/calldatabase.js
+++ routes/calldatabase.js
@@ -2,54 +2,69 @@
 
 var CallDatabase = function ( io, db ) {
  
-    var callDB = require('../calldatastore')(db, 'callstore');
+    var callDB = require('../calldatastore')(db);
     var database = io.of('/calldatabase');
  
     database.on('connection', function(socket) {
  
-        socket.on('message', function(got) {
-	 console.log(got);
-
-            io.of('/calldatabase').emit('message', 'sent');
-        });
-        socket.on('getAllTodos', function() {
-            dispatchAll(socket);
-        });
-        socket.on('listall', function() {
-           callDB.listall(function(err, data) {
+	    socket.on('listall', function(listdata) {
+			var recname = listdata.recordname;
+			
+           callDB.listall(recname,listdata.query, function(err, data) {
             if (err) throw err; // You can emit the error to a socket 
             io.of('/calldatabase').emit('listall', data);
            })
         });
  
-        socket.on('push', function(todo) {
-            callDB.push(todo, function(err, data) {
+        socket.on('push', function(pushdata) {
+			var recname = pushdata.recordname;
+			
+            callDB.push(recname, pushdata.data, function(err, data) {
                 if (err) throw err; // You can emit the error to a socket	
+				io.of('/calldatabase').emit('returndata', data);
             });
          });
-        socket.on('saveTodo', function(todo) {
-            callDB.saveTodo(todo, function(err, data) {
-                if (err) throw err; // You can emit the error to a socket	
-                dispatchAll(socket);
-            });
-        });
- 
-        socket.on('updateTodo', function(data) {
-            callDB.updateTodo(data, function(err, data) {
+      
+		socket.on('get', function(getdata) {
+			var recname = getdata.recordname;
+            callDB.get(recname,getdata.id, function(err, data) {
+				console.log('in get call' + data);
                 if (err) throw err; // You can emit the error to a socket 
-                dispatchAll(socket);
+                io.of('/calldatabase').emit('gotdata', data);
             });
         });
         
-        socket.on('deleteTodo', function(data) {
-            callDB.deleteTodo(data.id, function(err, data) {
+
+		
+        socket.on('update', function(updatedata) {
+			var recname = updatedata.recordname;
+            callDB.updateTodo(recname,updatedata.id, updatedata.data, function(err, data) {
                 if (err) throw err; // You can emit the error to a socket 
-                dispatchAll(socket);
+                io.of('/calldatabase').emit('returndata', data);
+            });
+        });
+        
+		
+		socket.on('remove', function(datatoremove) {
+			var recname = datatoremove.recordname;
+            callDB.remove(recname, datatoremove.id, function(err, data) {
+                if (err) throw err; // You can emit the error to a socket 
+                io.of('/calldatabase').emit('returndata', data);
+            });
+        });
+		
+		socket.on('removeall', function(datatoremove) {
+			var recname = datatoremove.recordname;
+            callDB.removeall(recname,  function(err, data) {
+                if (err) throw err; // You can emit the error to a socket 
+				
+                io.of('/calldatabase').emit('returndata', data);
             });
         });
  
-        // On connection send all the database, to save one round trip
-        dispatchAll(socket);
+		
+ 
+    
     });
  
  
